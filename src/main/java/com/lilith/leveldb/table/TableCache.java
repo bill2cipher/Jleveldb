31b9e8a32b8725a5d1f2f88058b352478615f93b
jellybean4 <xulongfetion@gmail.com>
2015-12-15 12:00:25 +0800

    table cache finished

--- src/main/java/com/lilith/leveldb/table/TableCache.java
+++ src/main/java/com/lilith/leveldb/table/TableCache.java
@@ -1,47 +1,69 @@
 package com.lilith.leveldb.table;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import com.lilith.leveldb.api.Slice;
+import com.lilith.leveldb.exceptions.DecodeFailedException;
 import com.lilith.leveldb.util.BinaryUtil;
+import com.lilith.leveldb.util.FileName;
 import com.lilith.leveldb.util.Options;
-import com.lilith.leveldb.util.ReadOptions;
 import com.lilith.leveldb.util.Settings;
 
 public class TableCache {
   
   private String dbname = null;
   private Options options = null;
-  private Cache cache = null;
-  
-  public class Iterator {
-    
+  private Cache<Slice, TableAndFile> cache = null;
+
+  private class TableAndFile {
+    public Table table = null;
+    public DataInputStream reader = null;
+    public TableAndFile(Table table, DataInputStream reader) {
+      this.table = table;
+      this.reader = reader;
+    }
   }
-  
+
   public TableCache(String dbname, Options options, int entries) {
     this.dbname = dbname;
     this.options = options;
-    this.cache = new LRUCache(entries);
+    this.cache = new ShardedLRUCache<Slice, TableAndFile>(entries);
   }
-  
+
   /**
-   * Return an iterator for the specified file number (the corresponding file length must be exactly 'file_size' bytes).
+   * If a seek to internal key in specified file finds an entry, null if not
+   * found.
+   * @throws DecodeFailedException 
+   * @throws IOException 
    */
-  public Iterator TableCacheIterator(ReadOptions options, long file_num, int file_size) {
-    return new Iterator();
-  }
-  
-  /**
-   * If a seek to internal key in specified file finds an entry, null if not found.
-   */
-  public Slice Get(ReadOptions options, long file_num, int file_size, Slice intern_key) {
-    Table table = FindTable(file_num, file_size);
+  public TableAndFile Get(long file_num, int file_size) throws IOException, DecodeFailedException {
+    return FindTable(file_num, file_size);
   }
   
   /**
    * Evict any entry for the specified file number
    */
   public void Evict(long file_num) {
-   byte[] buffer = new byte[Settings.UINT64_SIZE];
-   BinaryUtil.PutVarint64(buffer, 0, file_num);
-   cache.Erase(new Slice(buffer));
+    byte[] buffer = new byte[Settings.UINT64_SIZE];
+    BinaryUtil.PutVarint64(buffer, 0, file_num);
+    Slice key = new Slice(buffer);
+    cache.Erase(key, key.hashCode());
+  }
+
+  private TableAndFile FindTable(long file_num, int file_size) throws IOException, DecodeFailedException {
+    byte[] buffer = new byte[Settings.UINT64_SIZE];
+    BinaryUtil.PutVarint64(buffer, 0, file_num);
+    Slice key = new Slice(buffer);
+    TableAndFile value = cache.Lookup(key, key.hashCode());
+    if (value == null) {
+      String table_name = FileName.LogFileName(dbname, file_num);
+      DataInputStream reader = new DataInputStream(new FileInputStream(table_name));
+      Table table = Table.Open(reader, file_size);
+      value = new TableAndFile(table, reader);
+      cache.Insert(key, value, 1, key.hashCode());
+    }
+    return value;
   }
 }
